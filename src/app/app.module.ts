547135b44858cd1875fb42c08e64ed7d7b242101
Inma Gijón <igijoninf@gmail.com>
2022-06-02 17:26:42 +0200

    Rutas de la aplicación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRouterModule } from './app-router.module';
 
 import { SharedModule } from './shared/shared.module';
 
@@ -13,7 +14,8 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
-    SharedModule
+    SharedModule,
+    AppRouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
