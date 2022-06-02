d916f2dfb21c11f8bcad415690f4713e79281cbf
Inma Gijón <igijoninf@gmail.com>
2022-06-02 16:00:55 +0200

    Introducción a pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesApp';
+  nombre: string = 'InMA gijÓn';
+  valor: number = 1000;
+  obj = {
+    nombre: this.nombre
+  }
+
+  mostrarNombre() {
+    console.log(this.nombre);
+    console.log(this.valor);
+    console.log(this.obj);
+
+  }
 }
